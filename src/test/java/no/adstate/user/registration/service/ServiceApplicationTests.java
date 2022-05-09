efa9bc7707fbbb3acc2a91343209fee617d43019
Swapnil <swapnil1redekar@gmail.com>
2022-05-09 03:55:50 +0200

    Minor changes in Test class

--- src/test/java/no/adstate/user/registration/service/ServiceApplicationTests.java
+++ src/test/java/no/adstate/user/registration/service/ServiceApplicationTests.java
@@ -3,10 +3,10 @@ package no.adstate.user.registration.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ServiceApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
